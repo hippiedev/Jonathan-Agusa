8351a6599afb0ec4df6f685d4102e8d4dcffeac2
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-04-28 20:58:07 +0200

    SEO optimizations

--- src/template/post.js
+++ src/template/post.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { graphql } from "gatsby";
 import "../styles/Post.scss";
-import Helmet from 'react-helmet';
-import Img from 'gatsby-image';
+import Helmet from "react-helmet";
+import Img from "gatsby-image";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 export default function postTemplate({ data }) {
   const { markdownRemark: post } = data;
+  const image = <Img fluid={post.frontmatter.thumb.childImageSharp.fluid} />;
   return (
     <div>
       <Helmet>
-        <script src="https://kit.fontawesome.com/8c8a5b4cb9.js" crossorigin="anonymous"></script>
-        </Helmet>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={post.frontmatter.title} />
+        <meta name="twitter:description" content={post.frontmatter.description} />
+        <meta name="twitter:image" content={image} />
+      <meta property="og:title" content={post.frontmatter.title} />
+        <meta property="og:description" content={post.frontmatter.description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:type" content="article" />
+        <meta name="image" content={image} />
+        <meta name="description" content={post.frontmatter.description} />
+        <title>{post.frontmatter.title}</title>
+        <script
+          src="https://kit.fontawesome.com/8c8a5b4cb9.js"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
       <Header />
       <div className="Post">
         <h1>{post.frontmatter.title}</h1>
         <span>{post.frontmatter.date}</span>
-        <Img style={{position: "relative", marginTop: "20px", height: "250px"}} fluid={post.frontmatter.thumb.childImageSharp.fluid} />
+        <Img
+          style={{ position: "relative", marginTop: "20px", height: "250px" }}
+          fluid={post.frontmatter.thumb.childImageSharp.fluid}
+        />
         <div
           className="Content"
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -35,6 +53,7 @@ export const postTemplateQuery = graphql`
       frontmatter {
         path
         title
+        description
         date(fromNow: true)
         thumb {
           childImageSharp {
@@ -47,5 +66,4 @@ export const postTemplateQuery = graphql`
       id
     }
   }
-
 `;
