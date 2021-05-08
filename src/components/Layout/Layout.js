cd351bb2771109839b3121dc09ecd4bf34ea85ea
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-05-08 15:52:22 +0200

    Some changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,12 +13,17 @@ function Layout({ children }) {
         <Helmet>
         <title>{title}</title>
         <meta name="description" content={description}/>
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta
           name="twitter:description"
           content={description}
         />
         <meta name="twitter:image" content={image} />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content={description} />
         <script src="https://kit.fontawesome.com/8c8a5b4cb9.js" crossorigin="anonymous"></script>
         </Helmet>
            <Header /> 
