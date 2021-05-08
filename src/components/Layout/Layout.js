68dbcdb7f099b41b0da2b113bfa6e2bb2688379e
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-05-08 15:44:42 +0200

    Some changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,22 @@ import Helmet from 'react-helmet';
 import './Layout.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import image from '../../images/manhattan.jpg';
 
 function Layout({ children }) {
+    let description = "My name is Jonathan Agusa and this is my portfolio site";
+    let title = "Jonathan"; 
     return (
         <div className="Layout" >
         <Helmet>
-        <title>Jonathan</title>
-        <meta name="description" content="My name is Jonathan Agusa and this is my portfolio site" />
+        <title>{title}</title>
+        <meta name="description" content={description}/>
+        <meta name="twitter:title" content={title} />
+        <meta
+          name="twitter:description"
+          content={description}
+        />
+        <meta name="twitter:image" content={image} />
         <script src="https://kit.fontawesome.com/8c8a5b4cb9.js" crossorigin="anonymous"></script>
         </Helmet>
            <Header /> 
