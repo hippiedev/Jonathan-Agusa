0b93fdd579e8eca0132eb5f4ac1141f3b4d87cab
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-05-12 19:01:30 +0200

    Favicon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,19 +3,18 @@ import Helmet from 'react-helmet';
 import './Layout.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import metaImage from '../../images/manhattan.jpg';
 
 function Layout(props) {
     let description = props.description ? props.description : "My name is Jonathan Agusa and this is my portfolio site";
     let title = props.title ? props.title : "Jonathan";
-    let image = props.image ? props.image : metaImage;
+    let image = props.image ? props.image : "https://res.cloudinary.com/creattors/image/upload/v1620837673/IMG_-5jiov6_myc7ss.jpg";
     
     return (
         <div className="Layout" >
         <Helmet>
         <title>{title}</title>
         <meta name="description" content={description}/>
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta
           name="twitter:description"
@@ -26,6 +25,7 @@ function Layout(props) {
         <meta
           property="og:description"
           content={description} />
+        <link rel="icon" href="../../images/icon.png" />
         <script src="https://kit.fontawesome.com/8c8a5b4cb9.js" crossorigin="anonymous"></script>
         </Helmet>
            <Header/> 
